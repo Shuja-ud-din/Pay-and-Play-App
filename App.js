961868edae886f1790e3d1af62989a9da03271f1
Shuja ud Din <shuja1339@gmail.com>
2024-03-21 00:24:04 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Play from './src/screens/Play';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StripeProvider
+      publishableKey="pk_test_51OvxmMP7aIf9ZJPYf0blIQlE3wsqJthcSUYCCRCpwRvSXNidDYpoyfBR1ZzJXd9rYxKCgNC4vMVQYsQEswO7rPJq00bmhHCLue"
+      urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
+      merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}" // required for Apple Pay
+    >
+      <View style={styles.container}>
+        <Play />
+      </View>
+    </StripeProvider>
   );
 }
 
